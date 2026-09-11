e61a5933c8fd45928caa491fcddd8aaaee1e777f
tracker <moviestudy900@gmail.com>
2026-09-12 03:32:40 +0600

    Add files via upload

--- app/src/main/java/com/myapp/tracker/MainActivity.kt
+++ app/src/main/java/com/myapp/tracker/MainActivity.kt
@@ -1,1 +1,31 @@
+package com.myapp.tracker
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.myapp.tracker.data.HomeViewModel
+import com.myapp.tracker.ui.HomeScreen
+import com.myapp.tracker.ui.theme.AppBackground
+import com.myapp.tracker.ui.theme.TrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TrackerApp()
+        }
+    }
+}
+
+@Composable
+fun TrackerApp() {
+    TrackerTheme {
+        Surface(color = AppBackground) {
+            val viewModel: HomeViewModel = viewModel()
+            HomeScreen(viewModel = viewModel)
+        }
+    }
+}
